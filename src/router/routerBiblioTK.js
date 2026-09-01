ea9bc1642329c3e6d0337a977d0868b32ef158a3
andresfhenao <andresfelipehenaor@gmail.com>
2026-08-31 22:38:11 -0500

    maikol es gay

--- src/router/routerBiblioTK.js
+++ src/router/routerBiblioTK.js
@@ -2,7 +2,7 @@ import { Router } from 'express'
 import { Registro } from '../controllers/registroController.js'
 
 const router = Router()
-
+//maikol es gay
 router.post('/Registro', Registro)
 
 export default router
